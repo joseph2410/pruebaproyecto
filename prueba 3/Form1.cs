50caf56d216c0cae6fd0cee058d01ba01503ef9a
joseph2410 <josephjacinto@hotmail.es>
2015-10-07 14:46:00 -0500

    SEGUNDO COMMIT

--- prueba 3/Form1.cs
+++ prueba 3/Form1.cs
@@ -16,5 +16,10 @@ namespace prueba_3
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("LOCO MARICON");
+        }
     }
 }
